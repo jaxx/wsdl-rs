e913b2bc9d022bbead40ced48b256b248da8d1e0
Jakko Sikkar <jakko.sikkar@gmail.com>
2016-07-04 16:04:16 +0300

    trying to parse wsdl content

--- src/wsdl/lib.rs
+++ src/wsdl/lib.rs
@@ -12,28 +12,45 @@ use encoding::all::UTF_8;
 use encoding::DecoderTrap;
 use encoding::types::decode;
 
+const NAMESPACE_WSDL: &'static str = "http://schemas.xmlsoap.org/wsdl/";
+
 pub struct Wsdl {
+    //operations: Vec<WsdlOperation>
+    //types: Vec<WsdlType> 
+}
+
+struct WsdlType {
     
+}
+
+struct WsdlOperation {
+
 }
 
 impl Wsdl {
     pub fn load_from_url(url: &str) -> HttpResult<Wsdl> {
         let contents = try!(http::get(url));
         let decoded_contents = &decode_contents(contents)[..];
+
         let parser = EventReader::new(decoded_contents);
 
         for element in parser {
             match element {
-                Ok(XmlEvent::StartElement { name, .. }) => {
-                    println!("{:?}", name);
+                Ok(XmlEvent::StartElement { ref name, .. }) => {
+                    if name.local_name == "definitions" {
+                        println!("Found definitions element: {:?}", name);
+                    }
                 },
+                /*
                 Ok(XmlEvent::EndElement { name }) => {
                     println!("{:?}", name);
                 },
                 Err(e) => {
                     panic!("Error: {}", e);
                 }
+                */
                 _ => {}
+                
             }
         }
 
@@ -45,19 +62,24 @@ impl Wsdl {
     pub fn load_from_file(location: &str) -> IoResult<Wsdl> {
         let contents = try!(file::load(location));
         let decoded_contents = &decode_contents(contents)[..];
+
         let parser = EventReader::new(decoded_contents);
 
         for element in parser {
             match element {
-                Ok(XmlEvent::StartElement { name, .. }) => {
-                    println!("{:?}", name);
+                Ok(XmlEvent::StartElement { ref name, .. }) => {
+                    if name.local_name == "definitions" {
+                        println!("Found definitions element: {:?}", name);
+                    }
                 },
+                /*
                 Ok(XmlEvent::EndElement { name }) => {
                     println!("{:?}", name);
                 },
                 Err(e) => {
                     panic!("Error: {}", e);
                 }
+                */
                 _ => {}
             }
         }
@@ -70,7 +92,6 @@ impl Wsdl {
 
 fn decode_contents(bytes: Vec<u8>) -> Vec<u8> {
     let (decoded_contents, _) = decode(&bytes, DecoderTrap::Replace, UTF_8);
-    
 
     match decoded_contents {
         Ok(contents) => contents.as_bytes().to_vec(),
