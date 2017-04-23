6b8d56a55f6f7e57138cf0258b31020443e92cde
Janno Põldma <janno.poldma@gmail.com>
2017-04-23 20:02:17 +0300

    Refactor.

--- src/wsdl/lib.rs
+++ src/wsdl/lib.rs
@@ -6,6 +6,7 @@ mod http;
 mod file;
 
 use std::io::Result as IoResult;
+use std::fmt;
 use http::Result as HttpResult;
 use xml::reader::{EventReader, XmlEvent};
 use encoding::all::UTF_8;
@@ -27,66 +28,25 @@ struct WsdlOperation {
 
 }
 
+#[derive(Debug)]
+pub enum ParseError {
+    Http(hyper::Error),
+    Io(std::io::Error)
+}
+
+pub type ParseResult<T> = Result<T, ParseError>;
+
 impl Wsdl {
-    pub fn load_from_url(url: &str) -> HttpResult<Wsdl> {
-        let contents = http::get(url)?;
-        let decoded_contents = &decode_contents(contents)[..];
-
-        let parser = EventReader::new(decoded_contents);
-
-        for element in parser {
-            match element {
-                Ok(XmlEvent::StartElement { ref name, .. }) => {
-                    if name.local_name == "definitions" {
-                        println!("Found definitions element: {:?}", name);
-                    }
-                },
-                /*
-                Ok(XmlEvent::EndElement { name }) => {
-                    println!("{:?}", name);
-                },
-                Err(e) => {
-                    panic!("Error: {}", e);
-                }
-                */
-                _ => {}
-                
-            }
-        }
-
-        Ok(Wsdl {
-
-        })
+    pub fn load_from_url(url: &str) -> ParseResult<Wsdl> {
+        let contents = http::get(url).map_err(ParseError::Http)?;
+        let decoded_contents = decode_contents(contents);
+        parse_wsdl(&decoded_contents[..])
     }
 
-    pub fn load_from_file(location: &str) -> IoResult<Wsdl> {
-        let contents = file::load(location)?;
-        let decoded_contents = &decode_contents(contents)[..];
-
-        let parser = EventReader::new(decoded_contents);
-
-        for element in parser {
-            match element {
-                Ok(XmlEvent::StartElement { ref name, .. }) => {
-                    if name.local_name == "definitions" {
-                        println!("Found definitions element: {:?}", name);
-                    }
-                },
-                /*
-                Ok(XmlEvent::EndElement { name }) => {
-                    println!("{:?}", name);
-                },
-                Err(e) => {
-                    panic!("Error: {}", e);
-                }
-                */
-                _ => {}
-            }
-        }
-
-        Ok(Wsdl {
-            
-        })
+    pub fn load_from_file(location: &str) -> ParseResult<Wsdl> {
+        let contents = file::load(location).map_err(ParseError::Io)?;
+        let decoded_contents = decode_contents(contents);
+        parse_wsdl(&decoded_contents[..])
     }
 }
 
@@ -96,5 +56,42 @@ fn decode_contents(bytes: Vec<u8>) -> Vec<u8> {
     match decoded_contents {
         Ok(contents) => contents.as_bytes().to_vec(),
         Err(e) => panic!("Failed to decode contents: {}", e)
+    }
+}
+
+fn parse_wsdl(decoded_contents: &[u8]) -> ParseResult<Wsdl> {
+    let parser = EventReader::new(decoded_contents);
+
+    for element in parser {
+        match element {
+            Ok(XmlEvent::StartElement { ref name, .. }) => {
+                if name.local_name == "definitions" {
+                    println!("Found definitions element: {:?}", name);
+                }
+            },
+            /*
+            Ok(XmlEvent::EndElement { name }) => {
+                println!("{:?}", name);
+            },
+            Err(e) => {
+                panic!("Error: {}", e);
+            }
+            */
+            _ => {}
+            
+        }
+    }
+
+    Ok(Wsdl {
+
+    })
+}
+
+impl fmt::Display for ParseError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &ParseError::Http(ref err) => write!(fmt, "{}", err),
+            &ParseError::Io(ref err) => write!(fmt, "{}", err)
+        }
     }
 }
